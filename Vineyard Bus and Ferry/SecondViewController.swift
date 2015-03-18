91de2450de3185050600233c5abe913311932cee
Frederick Hewett <fred.hewett@gmail.com>
2015-03-18 16:16:50 -0400

    Initial Commit

---
+++ Vineyard Bus and Ferry/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Vineyard Bus and Ferry
+//
+//  Created by Fred Hewett on 3/18/15.
+//  Copyright (c) 2015 Frederick Hewett. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Vineyard Bus and FerryTests/Vineyard_Bus_and_FerryTests.swift
@@ -0,0 +1,36 @@
+//
+//  Vineyard_Bus_and_FerryTests.swift
+//  Vineyard Bus and FerryTests
+//
+//  Created by Fred Hewett on 3/18/15.
+//  Copyright (c) 2015 Frederick Hewett. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Vineyard_Bus_and_FerryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
